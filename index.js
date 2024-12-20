3c34211f07996268642a903c9a97bbf11ccbde8f
mujtabasaqib <mujtabasaqib99@gmail.com>
2024-12-20 15:33:31 +0500

    added password hashing

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const User = require('./models/user');
 const port = 3000;
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const DATABASE_NAME = 'auth';
 const CONNECTION_STRING = `mongodb://localhost:27017/${DATABASE_NAME}`;
@@ -21,20 +22,23 @@ mongoose.connect(CONNECTION_STRING)
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+app.get('/', (req, res) => {
+    res.send("Home Page");
+})
+
 app.get('/register', (req, res) => {
     res.render('register')
 })
 
 app.post('/register', async (req, res) => {
-  /*
-    const user = new User({
-        username: req.body.username,
-        password: req.body.password
-    })
-
-    await user.save()
-    */
-    res.send(req.body);
+   const {username, password} = req.body;
+   const hashedPassword = await bcrypt.hash(password, 12);
+   const user = new User({
+    username,
+    password: hashedPassword
+})
+   await user.save()
+   res.redirect('/');
 });
 
 app.get('/secret', (req, res) => {
